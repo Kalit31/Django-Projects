c3bbe3eb515474b8b32279b93d4f29f117203921
Kalit31 <inanikalit31@gmail.com>
2019-08-03 18:22:15 +0530

    implemented checkout feature.

--- e_store/store/urls.py
+++ e_store/store/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
   path('',views.catalog, name = "catalog"),
   path('cart/',views.cart,name="cart"),
   path('cart/remove/',views.removefromcart,name="remove"),
+  path('cart/checkout/',views.checkout,name="checkout"),
+  path('cart/checkout/complete/',views.completeOrder)
 ]
